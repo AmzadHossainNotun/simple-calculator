6ac7b3b3f65f1a108c43fa59e652d7d1c1200f09
Md. Amzad Hossain Notun <110979018+AmzadHossainNotun@users.noreply.github.com>
2024-02-06 00:48:25 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+# Basic Calculations
+# Calculator Program in Python by using input() and format() functions
+
+#Promting input from the user
+
+n1 = float(input("Enter the First Number: "))
+n2 = float(input("Enter the Second Number: "))
+
+#addition
+
+print("{} + {} = ".format(n1, n2))
+print(n1 + n2)
+
+#subtraction
+
+print("{} - {} = ".format(n1, n2))
+print(n1 - n2)
+
+#multiplication
+
+print("{} * {} = ".format(n1, n2))
+print(n1 * n2)
+
+#division
+
+print("{} / {} = ".format(n1, n2))
+print(n1 / n2)
